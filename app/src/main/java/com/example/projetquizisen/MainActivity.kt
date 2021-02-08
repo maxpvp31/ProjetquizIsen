fe695d543b4221866b5e6f84c092ec08fc378406
maxpvp31 <loubet.maxime31@gmail.com>
2021-02-08 20:22:08 +0100

    hhtp call work

--- app/src/main/java/com/example/projetquizisen/MainActivity.kt
+++ app/src/main/java/com/example/projetquizisen/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.projetquizisen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+// ...
+
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        run("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+                .url(url)
+                .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+        })
     }
 }
